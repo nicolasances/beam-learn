01375eaf82ea3c00b336fc5e8f63fc28e89bbda8
nicolasances <nicolas.matteazzi@gmail.com>
2022-12-26 18:34:48 +0100

    sd

--- mongo/pipeline.py
+++ mongo/pipeline.py
@@ -20,7 +20,7 @@ def main(argv=None, save_main_session=True):
 
     with beam.Pipeline(options = beam_options) as p: 
 
-        p | ReadFromMongoDB(uri = "mongodb+srv://{known_args.mongouser}:{known_args.mongopwd}@pleggit-play-cluster.2xyhv.mongodb.net/admin", db="profile", coll="profiles")
+        p | ReadFromMongoDB(uri = "mongodb+srv://{usr}:{pwd}@pleggit-play-cluster.2xyhv.mongodb.net/admin".format(usr = known_args.mongouser, pwd = known_args.mongopwd), db="profile", coll="profiles")
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
